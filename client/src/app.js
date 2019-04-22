3a6dbd0843821abc95d09656378c515094079d6a
Henadzi Zubaryk <h.zubaryk@godeltech.com>
2019-04-22 18:08:45 +0300

    save data to data obj

--- client/src/app.js
+++ client/src/app.js
@@ -6,7 +6,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
   const textArea = getElement("#textinput");
   const encrypt = getElement("#encrypt");
   const decrypt = getElement("#decrypt");
-  const data = { id: null, encryptedText: null };
+  const input = getElement("#input");
+  let data = { id: null, encrypted_text: null };
 
   const createAndEncrypt = async data => {
     try {
@@ -31,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
   const decryptText = async data => {
     try {
       const req = new Request();
-      const body = JSON.stringify({ default_text: data.encryptedText })
+      const body = JSON.stringify({ default_text: data.encrypted_text })
       return await req.get(`http://localhost:8000/api/${data.id}/`, body)
     } catch (error) {
       throw new Error(error)
@@ -41,14 +42,16 @@ document.addEventListener("DOMContentLoaded", function (event) {
   encrypt.addEventListener("click", async e => {
     e.preventDefault();
     const response = await createAndEncrypt(textArea.value);
-    data.id = response.id;
+    data = response;
     const encryptedText = response.encrypted_text;
     textArea.value = encryptedText;
+    console.log(data);
   });
 
   decrypt.addEventListener("click", async e => {
     e.preventDefault();
     const response = await decryptText(data);
+    data = response;
     textArea.value = response.encrypted_text;
     console.log(data);
   });
